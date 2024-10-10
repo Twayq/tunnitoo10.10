22718336c5ff5be7150d6ee21a26030307c7da11
GrigoriLysenko <Lysenkofn@gmail.com>
2024-10-10 11:22:31 +0300

    Tunnitöö lahedatud, samuti ka praktiline töö (Koos Rasmusega T.)

--- praktiline_too_10.10.py
+++ praktiline_too_10.10.py
@@ -8,7 +8,13 @@ b = float(input("Sisesta oma b:"))
 # koosta muutuja a, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
 # koosta muutuja b, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
 # meie ülesandeks on leida hüpoteenus c, kolmnurga ümbermõõt ja pindala NB(vastused peavad olema ümardatud sajandikeni)
-hupotenuusC = math.sqrt((a ** 2) + (b ** 2))
-P = a + b + hupotenuusC
-S = (a * b) / 2
+c = math.sqrt((a ** 2) + (b ** 2))
+p = a + b + c
+s = (a * b) / 2
+print(f"Kolmnurga hüpotenuus c={round(c, 2)}")
+print(f"Kolmnurga ümbermõõt p={round(p, 2)}")
+print(f"Kolmnurga pindala c={round(s, 2)}")
+
+
+
 
